6b7a50968e822b9687812a4191a9650083125ef4
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-01-25 22:07:09 -0800

    implement simple game control inline within Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,12 +1,47 @@
-import { ReactNode } from "react";
+import { ReactNode, useCallback, useState } from "react";
+import canAddToColumn from "../gameLogic/canAddToColumn";
+import isWinningMove from "../gameLogic/isWinningMove";
 
 import newGameState from "../gameLogic/newGameState";
 
-import Square from "./Square";
+import Square, { chipColorMap, SquareState } from "./Square";
 
 const Board = () => {
-  const cols = newGameState();
-  const squareElements = cols.reduce((squares, col, colIndex) => {
+  const [gameState, setGameState] = useState(newGameState());
+  const [isRedTurn, setIsRedTurn] = useState(true);
+
+  const makeMove = (c: number) => {
+    const r = canAddToColumn(c, gameState);
+    if (r === false) {
+      console.warn(`invalid move; col ${c} may be full`);
+      return;
+    }
+    const color = isRedTurn ? SquareState.Red : SquareState.Black;
+    if (isWinningMove(color, c, gameState)) {
+      alert(`${color} wins!`);
+      setGameState(newGameState());
+      setIsRedTurn(true);
+    } else {
+      gameState[c] = [...gameState[c]];
+      gameState[c][r] = color;
+      setGameState([...gameState]);
+      setIsRedTurn(!isRedTurn);
+    }
+  };
+
+  const buttons = [...new Array(gameState.length).keys()].map((i) => {
+    return (
+      <button
+        key={`b${i}`}
+        onClick={() => makeMove(i)}
+        className={
+          chipColorMap[isRedTurn ? SquareState.Red : SquareState.Black]
+        }
+      ></button>
+    );
+  });
+
+  const squareElements = gameState.reduce((squares, col, colIndex) => {
     col.forEach((squareState, rowIndex) => {
       squares.push(
         <Square key={`${rowIndex}${colIndex}`} state={squareState} />
@@ -16,9 +51,12 @@ const Board = () => {
   }, new Array<ReactNode>());
 
   return (
-    <div className="w-196 h-168 flex flex-wrap flex-col-reverse">
-      {squareElements}
-    </div>
+    <>
+      <div className="w-196 flex justify-around">{buttons}</div>
+      <div className="w-196 h-168 flex flex-wrap flex-col-reverse">
+        {squareElements}
+      </div>
+    </>
   );
 };
 
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -8,7 +8,7 @@ interface SquareProps {
   state: SquareState;
 }
 
-const bgClassMap: Record<SquareState, string> = {
+export const chipColorMap: Record<SquareState, string> = {
   [SquareState.Empty]: "bg-white",
   [SquareState.Red]: "bg-red-500",
   [SquareState.Black]: "bg-black",
@@ -17,7 +17,7 @@ const bgClassMap: Record<SquareState, string> = {
 const Square = ({ state }: SquareProps) => {
   return (
     <span className="w-28 h-28 border-solid border-2 border-yellow-400 flex items-center justify-center bg-yellow-300">
-      <div className={`w-24 h-24 rounded-full ${bgClassMap[state]}`} />
+      <div className={`w-24 h-24 rounded-full ${chipColorMap[state]}`} />
     </span>
   );
 };
